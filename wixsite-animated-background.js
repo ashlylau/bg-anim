fd5f3eb1fabc7d6d75faaec30ec19b6294959e12
Ashly Lau <37477781+ashlylau@users.noreply.github.com>
2022-10-15 13:34:05 -0400

    width of footer fits page even if parent doesn't fit
    need to ensure that parent is centered. see https://css-tricks.com/full-width-containers-limited-width-parents/#aa-no-calc-needed

--- wixsite-animated-background.js
+++ wixsite-animated-background.js
@@ -1,23 +1,16 @@
-// import wixSite from 'wix-site';
-
-function createSectionBg(sectionHeight) {
+function createFooterBg(footerHeight) {
     const img = document.createElement('div');
-    img.id = "section-bg";
-    // img.style.position = 'relative';
-    // img.style.right = '50%';
-    img.style.left = '50';
+    img.id = "footer-bg";
+    img.style.position = "relative"
+    img.style.left = '50%';
     img.style.marginLeft = '-50vw';
-    // img.style.marginRight = '-50vw';
-    // img.style.margin = '0';
-    // img.style.right = '0';
+    img.style.right = '50%';
+    img.style.marginRight = '-50vw';
     img.style.top = '0';
-    // img.style.bottom = '0';
-    img.style.width = '150vw';
-    img.style.height = sectionHeight + 'px';
+    img.style.width = '100vw';
+    img.style.height = footerHeight + 'px';
     return img;
 }
-
-// var sectionHeight = 100;
 
 const blue = 'rgb(116, 182, 212, 0.6)';
 const blueBg = 'rgb(179, 205, 210, 0.001)';
@@ -29,57 +22,36 @@ function changeBackgroundGradient(x, y) {
     // console.log(`X: ${x}, Y: ${y}`);
     var width = window.innerWidth;
     var height = window.innerHeight;
-    var bgElement = document.getElementById('section-bg');
+    var bgElement = document.getElementById('footer-bg');
     console.log('bgElement: ' + bgElement);
 
     var xPercent = x/width*100;
     var yPercent = y/height*100;
 
-    // let pink1 = 'radial-gradient(at '+(xPercent**0.5+95)+'% '+(yPercent**0.7+5)+'%, '+pink+' 5%, transparent 40%)';
-    // let yellow1 = 'radial-gradient(at '+(yPercent**0.5+5)+'% '+(xPercent**0.8+95)+'%, '+yellow+' 0%, transparent 50%)';
-    // let green1 = 'radial-gradient(at '+(yPercent**0.7)+'% '+(xPercent**0.6)+'%, transparent 50%, '+green+' 70%, transparent 90%)';
-    // let blue1 = 'radial-gradient(circle at '+(xPercent**0.5)+'% '+(yPercent**0.6)+'%, '+blue+' 10%, transparent 65%)';
     let pink1 = 'radial-gradient(at '+(xPercent)+'% '+(yPercent)+'%, '+pink+', '+blue+')';
     let yellow1 = 'radial-gradient(at '+(20-yPercent**0.1)+'% '+(xPercent**0.2+55)+'%, '+yellow+', '+blue+')';
     let green1 = 'radial-gradient(at '+(yPercent**0.4-20)+'% '+(xPercent**0.2+20)+'%, '+green+' 20%, transparent 90%)';
     
     let radialGradient = pink1+','+yellow1+','+green1;
-    // let radialGradient = pink1+','+yellow1+','+green1+','+blue1;
     bgElement.style.background = radialGradient;
-    console.log(bgElement.style);
-    // bgElement.style.background = 'radial-gradient(at ' + xPercent +'% ' + yPercent + '%, #e66465, #9198e5)';
+    console.log('footer: ' + bgElement.style);
 }
 
-class BackgroundGradient extends HTMLElement {
+class FooterGradient extends HTMLElement {
         constructor() {
             super();
         }
         connectedCallback() {
             document.addEventListener('mousemove', (e) => {
-                console.log('mousemoved ' + e.clientX);
+                console.log('mousemoved footer' + e.clientX);
                 changeBackgroundGradient(e.clientX, e.clientY);
             });
 
-            // if home page, create section background. (y: 1072, height: 808)
-            // if (wixSite.currentPage.isHomePage) {
-                // this.appendChild(createSectionBg());
-            // }
-            // create footer background object for all pages except menu lightbox. (y: 3222, height: 860)
-            // if (wixSite.currentPage.type != 'lightbox') {
-            var thisHeight = this.offsetHeight;
-            console.log("section height: " + thisHeight);
-            var sectionHeight = this.offsetHeight;
-            this.appendChild(createSectionBg(sectionHeight));
-
-            // var bgAnimStrip = document.getElementById('#bgAnimStrip');
-            // console.log(bgAnimStrip);
-            // var sectionHeight = bgAnimStrip.offsetHeight;
-            // console.log(sectionHeight);
-            thisHeight = this.offsetHeight;
-            console.log("2: " + thisHeight);
-            const nodeList= document.querySelectorAll("p");
-            console.log(nodeList);
-            // }
+            var footerHeight = this.offsetHeight;
+            var footerWidth = this.offsetWidth;
+            console.log('footer width: ' + footerWidth);
+            console.log("footer height: " + footerHeight);
+            this.appendChild(createFooterBg(footerHeight));
         } 
 }
-customElements.define('background-gradient', BackgroundGradient);
+customElements.define('footer-gradient', FooterGradient);
